6c0cb8f3545614e914cde2c54948136c3fae92a0
Luis Crespo <lcrespom@gmail.com>
2021-06-28 14:59:05 +0200

    Parsing the XML

--- sporty.ts
+++ sporty.ts
@@ -1,4 +1,5 @@
 import { existsSync } from 'https://deno.land/std@0.99.0/fs/mod.ts'
+import { SAXParser, ElementInfo } from 'https://deno.land/x/xmlp@v0.2.8/mod.ts'
 
 // See https://decipher.dev/deno-by-example/advanced-readline/
 import { readLine } from 'https://raw.githubusercontent.com/deepakshrma/deno-by-example/master/examples/file_reader.ts'
@@ -19,6 +20,20 @@ async function filterXML(xmlPath: string, lineCB: LineCallback) {
     }
 }
 
+async function parseXML(xmlPath: string) {
+    const parser = new SAXParser()
+    parser
+        .on('start_element', (e: ElementInfo) => {
+            console.log('Start element:', e)
+        })
+        .on('end_element', (e: ElementInfo) => {
+            console.log('End element:', e)
+        })
+    const reader = await Deno.open(xmlPath)
+    await parser.parse(reader)
+    await reader.close()
+}
+
 
 async function main() {
     if (Deno.args.length < 1) {
@@ -33,11 +48,14 @@ async function main() {
     const tmpFile = Deno.makeTempFileSync()
     const encoder = new TextEncoder()
     const file = await Deno.open(tmpFile, { write: true })
+    await Deno.write(file.rid, encoder.encode('<Workouts>\n'))
     await filterXML(
         path,
         l => Deno.write(file.rid, encoder.encode(l + '\n'))
     )
-    Deno.close(file.rid);
+    await Deno.write(file.rid, encoder.encode('</Workouts>\n'))
+    await Deno.close(file.rid)
+    await parseXML(tmpFile)
     console.log('Check file ' + tmpFile)
 }
 
