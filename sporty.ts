8cbcf654455dfc186f45a07a8228b0ddf7e64cce
Luis Crespo <lcrespom@gmail.com>
2021-06-28 23:31:20 +0200

    XML to JSON

--- sporty.ts
+++ sporty.ts
@@ -1,5 +1,5 @@
 import { existsSync } from 'https://deno.land/std@0.99.0/fs/mod.ts'
-import { SAXParser, ElementInfo } from 'https://deno.land/x/xmlp@v0.2.8/mod.ts'
+import { SAXParser, ElementInfo, AttributeInfo } from 'https://deno.land/x/xmlp@v0.2.8/mod.ts'
 
 // See https://decipher.dev/deno-by-example/advanced-readline/
 import { readLine } from 'https://raw.githubusercontent.com/deepakshrma/deno-by-example/master/examples/file_reader.ts'
@@ -20,18 +20,44 @@ async function filterXML(xmlPath: string, lineCB: LineCallback) {
     }
 }
 
+type XMLObject = { [key: string]: string | XMLObject[] }
+
+function attrs2obj(attrs: AttributeInfo[]): XMLObject {
+    const obj: XMLObject = {}
+    for (const attr of attrs) {
+        obj[attr.qName] = attr.value
+    }
+    return obj
+}
+
 async function parseXML(xmlPath: string) {
     const parser = new SAXParser()
+    let inWorkout = false
+    let workout: XMLObject
+    const workouts: XMLObject[] = []
     parser
         .on('start_element', (e: ElementInfo) => {
-            console.log('Start element:', e)
+            if (inWorkout) {
+                if (!workout[e.qName]) {
+                    workout[e.qName] = [] as XMLObject[]
+                }
+                (workout[e.qName] as XMLObject[]).push(attrs2obj(e.attributes))
+            }
+            else {
+                if (e.qName != 'Workout') return
+                inWorkout = true
+                workout = attrs2obj(e.attributes)
+            }
         })
         .on('end_element', (e: ElementInfo) => {
-            console.log('End element:', e)
+            if (e.qName != 'Workout') return
+            inWorkout = false
+            workouts.push(workout)
         })
     const reader = await Deno.open(xmlPath)
     await parser.parse(reader)
     await reader.close()
+    return workouts
 }
 
 
@@ -55,8 +81,11 @@ async function main() {
     )
     await Deno.write(file.rid, encoder.encode('</Workouts>\n'))
     await Deno.close(file.rid)
-    await parseXML(tmpFile)
+    const workouts = await parseXML(tmpFile)
+    //Deno.remove(tmpFile)
+    console.log(JSON.stringify(workouts, null, 2))
     console.log('Check file ' + tmpFile)
+
 }
 
 
